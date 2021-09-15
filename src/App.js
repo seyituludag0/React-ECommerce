4840da56e77d7ce959b3cc1fce721963b05b6fe8
seyituludag0 <seyituludag0@gmail.com>
2021-09-16 00:23:42 +0300

    Navi created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css';
+import Navi from './layouts/navi/Navi';
+
+function App() {
+  return (
+    <div className="App">
+     <Navi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/navi/Navi.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import "../navi/navi.css";
+import logo from "../navi/img/logo.jpg"
+import search from "../navi/img/icon/search.png"
+import heart from "../navi/img/icon/heart.png"
+import cart from "../navi/img/icon/cart.png"
+
+export default function Navi() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-3">
+          <div className="header__logo">
+            <a routerLink="/">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6">
+          <nav className="header__menu mobile-menu">
+            <ul>
+              <li className="active">
+                <Menu.Item>
+                  <Link to="/asd">ANA SAYFA</Link>
+                </Menu.Item>
+              </li>
+
+              <li>
+                <a>SAYFALAR</a>
+                <ul className="dropdown">
+                <li>
+                    <Menu.Item>
+                      <Link>ÜRÜNLER</Link>
+                    </Menu.Item>
+                  </li>
+                  <li>
+                    <Menu.Item>
+                      <Link>KATEGORİLER</Link>
+                    </Menu.Item>
+                  </li>
+                  <li>
+                    <Menu.Item>
+                      <Link>HAKKIMIZDA</Link>
+                    </Menu.Item>
+                  </li>
+                  <li>
+                    <Menu.Item>
+                      <Link>İLETİŞİM</Link>
+                    </Menu.Item>
+                  </li>
+                  <li>
+                    <Menu.Item>
+                      <Link>ADMIN</Link>
+                    </Menu.Item>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-lg-3 col-md-3">
+          <div className="header__nav__option">
+            <a  className="search-switch">
+              <img src={search} alt="search-icon" />
+            </a>
+            <a >
+              <img src={heart} alt="heart-icon" />
+            </a>
+            <a >
+              <img src={cart} alt="cart-icon" /> <span>0</span>
+            </a>
+            <div className="price">$0.00</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
